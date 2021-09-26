8e6e40119278c947f891ac0ffc42958440d43270
MohammedKamle <66168802+MohammedKamle@users.noreply.github.com>
2021-09-26 18:36:17 +0530

    added reference type injection using constructor

---
+++ src/main/java/com/springcore/constructorinjection/Person.java
@@ -0,0 +1,36 @@
+package com.springcore.constructorinjection;
+
+public class Person {
+	private String name;
+	private int personId;
+	private Certificate certi;
+	
+	
+
+	public Person(String name, int personId, Certificate certi) {
+		super();
+		this.name = name;
+		this.personId = personId;
+		this.certi = certi;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return this.name+" : "+this.personId+" { "+this.certi.name+" }";
+	}
+	
+	
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/springcore/constructorinjection/Test.java
@@ -0,0 +1,18 @@
+package com.springcore.constructorinjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/constructorinjection/ciconfig.xml");
+		
+		Person person1 = (Person) context.getBean("person1");
+		Person person2 = (Person) context.getBean("person2");
+		
+		System.out.println(person1);
+		System.out.println(person2);
+	}
+
+}
