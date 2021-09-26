ed03b55e1343ee6720bff2cd65bf1f942c5c01ef
MohammedKamle <66168802+MohammedKamle@users.noreply.github.com>
2021-09-26 19:42:31 +0530

    modified main

--- src/main/java/com/springcore/constructorinjection/Test.java
+++ src/main/java/com/springcore/constructorinjection/Test.java
@@ -7,12 +7,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/constructorinjection/ciconfig.xml");
-		
-		Person person1 = (Person) context.getBean("person1");
-		Person person2 = (Person) context.getBean("person2");
-		
-		System.out.println(person1);
-		System.out.println(person2);
+	
 		
 		Addition addition = (Addition) context.getBean("addition");
 		addition.doSum();
