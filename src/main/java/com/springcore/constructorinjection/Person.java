464f68f61c161ad38c61d58711cd5a5a6d6f110d
MohammedKamle <66168802+MohammedKamle@users.noreply.github.com>
2021-09-26 19:00:19 +0530

    added collection injection through constructor-injection

--- src/main/java/com/springcore/constructorinjection/Person.java
+++ src/main/java/com/springcore/constructorinjection/Person.java
@@ -1,25 +1,36 @@
 package com.springcore.constructorinjection;
 
+import java.util.List;
+
 public class Person {
 	private String name;
 	private int personId;
 	private Certificate certi;
+	private List<String> hobbies;
 	
 	
 
-	public Person(String name, int personId, Certificate certi) {
+	public Person(String name, int personId, Certificate certi, List<String> hobbies ) {
 		super();
 		this.name = name;
 		this.personId = personId;
 		this.certi = certi;
+		this.hobbies = hobbies;
 	}
 
 
 
 	@Override
 	public String toString() {
-		return this.name+" : "+this.personId+" { "+this.certi.name+" }";
+		return "Person [name=" + name + ", personId=" + personId + ", certi=" + certi + ", hobbies=" + hobbies + "]";
 	}
+
+
+
+//	@Override
+//	public String toString() {
+//		return this.name+" : "+this.personId+" { "+this.certi.name+" } "+this.hobbies;
+//	}
 	
 	
 
